c807ef033c1ba7727a0ea2c349b971e54d9d77f8
jakelozano94 <jakelozano94@gmail.com>
2020-12-20 03:31:57 -0500

    added boilerplate repo listing

--- src/App.js
+++ src/App.js
@@ -11,24 +11,25 @@ import {
   useLocation
 } from 'react-router-dom'
 
+const clientId = "16980cfa0456b2c465d9"
+const authRoute = `https://github.com/login/oauth/authorize?scope=repo&client_id=${clientId}`
+const repoList = ``
+
 
 function App() {
 
-  const [authLink, setAuth] = useState("blah")
+  // const [authLink, setAuth] = useState()
 
- const showUsers = () => {
-   fetch('http://localhost:3000/signin')
-   .then(res=> res.json())
-   .then(res =>  {
-     console.log(setAuth)
-     setAuth(res)
-   })
+ const repoList = () => {
+   fetch('http://localhost:8000/repos/list')
+   .then(res => res.json())
+   .then(console.log)
    .catch(err => console.log(err))
  }
   return (
       <div className="hello">
-        <a href={authLink}>auth</a>
-        <button onClick={showUsers}>click</button>
+        <a href={authRoute}>auth</a>
+        <button onClick={repoList}>list repos</button>
       </div>
   );
 }
