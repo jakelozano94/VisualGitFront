e931c4bb4f1ae6332bc7362568b295d8b4808710
jakelozano94 <jakelozano94@gmail.com>
2021-01-25 23:43:54 -0500

    added comments for App.js

--- src/App.js
+++ src/App.js
@@ -20,41 +20,7 @@ const repoList = ``
 
 function App() {
 
-  const d3Tree = (data) => {
-    console.log(data)
-    let root = d3.stratify
-    let treemap = d3.treemap()
-      .size(['100px', '100px'])
-      .padding(2);
 
-    let nodes = treemap(root
-      .sum(function (data) { return data.author }))
-
-      const link = treemap.append("g")
-      .attr("fill", "none")
-      .attr("stroke", "#555")
-      .attr("stroke-opacity", 0.4)
-      .attr("stroke-width", 1.5)
-    .selectAll("path")
-      .data(root.links())
-      .join("path")
-        .attr("d", treemap);
-
-        node.append("circle")
-        .attr("r", 2.5);
-  
-    node.append("text")
-        .attr("dy", "0.31em")
-        .attr("x", d => d.children ? -6 : 6)
-        .attr("text-anchor", d => d.children ? "end" : "start")
-        .text(label)
-      .clone(true).lower()
-        .attr("stroke", "white");
-    
-    return svg.node();
-  }
-
-  // const [authLink, setAuth] = useState()
 
  const repoList = () => {
    fetch('http://localhost:8000/repos/list')
@@ -64,6 +30,7 @@ function App() {
  }
 
  const exampleRepo = () => {
+  // list a specific repo given from the back end
    fetch('http://localhost:8000/repos/example')
    .then(res => res.json())
    .then(console.log)
@@ -71,14 +38,15 @@ function App() {
  }
 
  const listCommits = () => {
+   // List comments from a repo of choice
    fetch('http://localhost:8000/repos/commits')
    .then(res => res.json())
-   .then(res => d3Tree(res.data))
    .then(console.log)
    .catch(console.log)
  }
 
  const listBranches = () => {
+   //pseudo code for listing all branches
    fetch('http://localhost:8000/repos/branches')
    .then(res => res.json())
    .then(console.log)
